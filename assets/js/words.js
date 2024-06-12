e0d927e89af92aeee3bdf8da9689e7f04266b3b9
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-06-12 04:48:48 -0500

    Added wordlist in a different file to make it easier for me to change

---
+++ assets/js/words.js
@@ -0,0 +1,27 @@
+/* I have not been able to find official word lists for the hacking minigame,
+So these were obtained by constantly entering and exiting terminals in Fallout 4
+May have duplicates because of that, hard to remember what I've already added */
+const words = {
+    'novice': [
+        "NAME", "TIRE", "HALL", "PAIN", "RATS", "MAZE", "KHANS", "SHOP", "GAVE", "DAMN", "SASH", "CRAP", "LAWS",
+        "BEDS", "AMMO", "WELL", "HOST", "HIGHS", "NEXT", "WISE", "WEST", "EAST", "SOUTH", "NORTH", "NONE",
+        "RUDE", "ROCK", "COOL", "COLD", "REST", "BORE", "CAST", "WARM", "TEST", "LAWS", "BEST", "SASH",
+        "LACK", "PETS", "SEEK", "PICK", "LETS", "NEWS", "GOOD", "LOOK", "SALT", "THEM", "AGED", "SEES",
+        "DEEM", "SKIN", "HERO", "DANK", "BELT", "KEEP", "TECH", "SPOT", "SEEK", "CORK", "FORK", "FUEL",
+        "NOTE", "HERE", "VERY", "FALL", "CAPE", "ZONE", "COST", "LOCK", "LAND", "HELD", "MEAT", "DENS",
+        "HAND", "WORD", "TIDY", "FARM", "ONLY", "SNOT", "PURE", "ONCE", "STAY", "CAIN", "JUKE", "EARL",
+        "FIRE", "TURN", "STIR", "WITH", "WALL", "BEAR", "NEON", "FACT", "SOME", "GONE", "SKIN", "HOME",
+        "TIME", "SURE", "BITS", "VATS", "MATE", "PACK", "PLOT", "HATE", "EACH", "EGOS", "NEXT", "PLUS"
+    ],
+    'expert': [
+        "REMAINED", "COMPARED", "ENORMOUS", "DETAILED", "GARRISON", "OVERCOME", "DARKNESS", "CONSISTS", "DISTRUST",
+        "TORTURED", "VILLAGER", "ENSLAVED", "SUBURBAN", "PRIORITY", "EGYPTIAN", "INSPIRED", "RESERVES", "ENGULFED",
+        "SEARCHED", "TRINKETS", "SUBJECTS", "UNCONVERS", "FREQUENT", "INVOLVED", "LAUNCHER", "UNLOCKED", "HARDWARE",
+        "SOFTWARE", "LANGUAGE", "PRODUCED", "UNMANNED", "OUTSIDER", "PRESENCE", "RUSTLING", "RHYTHMIC", "FINISHED",
+        "SECURITY", "STRONGER", "WARRIORS", "DECORATE", "PAINTING", "SAMANTHA", "GUARDIAN", "CONFLICT", "DIVISION",
+        "FOLLOWED", "TACTICAL", "DESPISES", "DECREPIT", "STRAIGHT", "JUMPSUIT", "KNUCKLES", "EXTERIOR", "INTERIOR",
+        "SURVIVOR", "GOVERNOR", "CARAVANS", "ARRESTED", "CONCRETE", "INTRUDER", "BOTHERED", "ARCHIVES", "CONTENTS",
+        "OFFICERS", "STANDING", "STATUARY", "ALTERING", "NORTHERN", "SOUTHERN", "WESTERN", "EASTERN", "EXPLAINS",
+        "GENETICS", "FAVORITE", "PLOTTING", "GATHERED", "PRIORITY", "FAMILIES"
+    ]
+};
